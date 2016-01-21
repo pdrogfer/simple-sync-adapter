1b61b498047eb2533fa2cc17cde853122fa680ad
pdrogfer <pdro74@hotmail.com>
2016-01-21 19:03:39 +0100

    using loopj to conect to web service

--- app/src/main/java/com/kavy/simplesyncadapter/MainActivity.java
+++ app/src/main/java/com/kavy/simplesyncadapter/MainActivity.java
@@ -2,13 +2,26 @@ package com.kavy.simplesyncadapter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
     String currentTime;
     TextView tvResults;
+    String answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +33,28 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickAddItem(View view) {
 
-        currentTime = String.valueOf(System.currentTimeMillis());
+        fetchDataFromWebServer();
 
-        tvResults.setText(currentTime);
+    }
+
+    private String fetchDataFromWebServer() {
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://192.168.1.4/SimpleSyncAdapter/get_time_json.php", new JsonHttpResponseHandler() {
+        // client.get("http://kavy.servehttp.com/SimpleSyncAdapter/get_time_json.php", new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                tvResults.setText(response.toString());
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.i(Utilities.TAG, "error: " + errorResponse.toString() );
+                answer = null;
+            }
+        });
+
+        return answer;
     }
 }
---
+++ app/src/main/java/com/kavy/simplesyncadapter/Utilities.java
@@ -0,0 +1,8 @@
+package com.kavy.simplesyncadapter;
+
+/**
+ * Created by pedro on 21/01/16.
+ */
+public class Utilities {
+    public static final String TAG = "app";
+}
